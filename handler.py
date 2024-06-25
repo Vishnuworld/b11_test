c79719d5510d3f22a63ddf8526510ad2f226a241
VishnuWorld <52811937+Vishnuworld@users.noreply.github.com>
2024-06-25 23:09:06 +0530

    added func1

--- handler.py
+++ handler.py
@@ -2,7 +2,11 @@ for i in range(1, 15):
     if i % 2 == 0:
         print(i)
 
-##############################################
+
+
+
+def func1():
+    print("edited on github")
 
 
 def outer(func):

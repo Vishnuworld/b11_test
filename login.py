3df28253d3110f3d8a1e8cf92869ccf6e63fabad
VishnuWorld <vrbhandari1995@gmail.com>
2024-06-26 22:17:43 +0530

    added docstring, constant

--- login.py
+++ login.py
@@ -1,8 +1,8 @@
-USERNAME = "ADMIN"
-PASSWORD = "admin123"
+from .constant import USERNAME, PASSWORD
 
 
 def login(user, password):
+    "user login"
     if user == USERNAME and password == PASSWORD:
         print("user logged in successfully.")
     else:
